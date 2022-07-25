835cfaebec525cf24a3172539b20dce741170bb0
adepane <adesbpane@gmail.com>
2022-07-25 23:35:02 +0700

    modify API to own proxy

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Banner from "../components/Banner/Banner";
+import Loader from "../components/Loader/Loader";
 import { useHotelDealsQuery } from "../services/hotelApi";
 
 const Home = () => {
   const { data = [], isLoading, isError } = useHotelDealsQuery();
-  const loaded = isLoading ? console.log("Loading...") : console.log(data);
+  const loaded = isLoading ? <Loader /> : console.log(data);
     return (
       <>
-      {loaded}
         <Banner />
+        <div className="flex h-screen">
+          {loaded}
+        </div>
       </>
     );
 }
--- src/services/hotelApi.js
+++ src/services/hotelApi.js
@@ -1,22 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_KEY = process.env.REACT_APP_IMPALA_KEY;
-console.log(API_KEY);
 export const hotelApi = createApi({
   reducerPath: "hotelApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://sandbox.impala.travel/v1/",
-    prepareHeaders: (headers) => {
-      headers.set("x-api-key", API_KEY );
-      headers.set("Access-Control-Allow-Origin", "*" );
-      headers.set("Content-Type", "application/json" );
-      return headers;
-    },
+    baseUrl: "https://hotel-api.blubit.xyz/api/",
   }),
   endpoints: (builder) => ({
     hotelDeals: builder.query({
-        
-      query: () => `hotels`,
+      query: () => `/hotels`,
     }),
   }),
   overrideExisting: false,
